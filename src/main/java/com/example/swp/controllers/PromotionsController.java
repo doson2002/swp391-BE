9d6dab5891977c6f9adcbf819e1318837fc1dabf
doson200220 <153179873+doson200220@users.noreply.github.com>
2024-06-17 20:50:58 +0700

    FEATURE: Fix promotions

--- src/main/java/com/example/swp/controllers/PromotionsController.java
+++ src/main/java/com/example/swp/controllers/PromotionsController.java
@@ -5,6 +5,7 @@ import com.example.swp.entities.Promotions;
 import com.example.swp.services.IPromotionsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -12,29 +13,33 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/promotions")
+@RequestMapping("/api/v1/promotions")
 public class PromotionsController {
     private final IPromotionsService promotionsService;
 
     @PostMapping("/create")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_MANAGER','STAFF')")
     public ResponseEntity<Promotions> createPromotions(@RequestBody PromotionsDTO promotionsDTO) {
         Promotions promotion = promotionsService.createPromotion(promotionsDTO);
         return ResponseEntity.ok(promotion);
     }
 
 
-    @GetMapping("/promotions")
+    @GetMapping("/get_all_promotions")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_MANAGER','STAFF')")
     public List<Promotions> getAllPromotions() {
         return promotionsService.getAllPromotions();
     }
 
     @DeleteMapping("/delete")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_MANAGER','STAFF')")
     public ResponseEntity<Void> deleteExpiredPromotions() throws Exception {
         promotionsService.deleteExpiredPromotions();
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/use/{code}")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_MANAGER','STAFF')")
     public ResponseEntity<Promotions> usePromotion(@PathVariable String code) {
         try {
             Promotions promotion = promotionsService.usePromotion(code);
